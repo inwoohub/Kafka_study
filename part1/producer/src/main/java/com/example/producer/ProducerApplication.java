5a171ec31595bac6a7907030aa68cd19fb2d45c2
inwoohub <dlsdndls@naver.com>
2026-04-18 17:11:01 +0900

    fix : docker-compse 수정 및 @EnableScheduling 어노테이션 추가

--- part1/producer/src/main/java/com/example/producer/ProducerApplication.java
+++ part1/producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -2,7 +2,9 @@ package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling // 스케줄링을 사용하므로 여기서도 스케줄링 추가하
 @SpringBootApplication
 public class ProducerApplication {
 
